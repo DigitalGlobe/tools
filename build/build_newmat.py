7d658be6b0f8e2519c8520ee19a65f1c6ba4d25c
Christopher McGeorge <cmcgeorge@surgeforward.com>
2016-07-27 15:52:26 -0700

    Wrote a Python script to build Newmat.

---
+++ build/build_newmat.py
@@ -0,0 +1,174 @@
+#------------------------------------------------------------------------------
+#
+# build_newmat.py
+#
+# Summary : Builds the Newmat library.
+#
+#------------------------------------------------------------------------------
+
+import glob
+import os
+import sys
+
+from BuildSettingSet import *
+from PathFinder import *
+from SystemManager import *
+
+#------------------------------------------------------------------------------
+# The Program class represents the main class of the script.
+class Program :
+
+    #--------------------------------------------------------------------------
+    # constants
+    
+        #----------------------------------------------------------------------
+        # a description of what the script does
+        DESCRIPTION = "Builds the Newmat library."
+        #----------------------------------------------------------------------
+        
+        #----------------------------------------------------------------------
+        # the name of the build debug library file
+        _FILE_NAME_LIBRARY_BUILD_DEBUG = "newmat.lib"
+        #----------------------------------------------------------------------
+        # the name of the distribution debug library file
+        _FILE_NAME_LIBRARY_DISTRIBUTION_DEBUG = "newmat_d.lib"
+        #----------------------------------------------------------------------
+        # the name of the build release library file
+        _FILE_NAME_LIBRARY_BUILD_RELEASE = "newmat.lib"
+        #----------------------------------------------------------------------
+        # the name of the distribution release library file
+        _FILE_NAME_LIBRARY_DISTRIBUTION_RELEASE = "newmat.lib"
+        #----------------------------------------------------------------------
+        # the name of the makefile
+        _FILE_NAME_MAKEFILE = "nm_m8.mak"
+        #----------------------------------------------------------------------
+        # the pattern for binary files
+        _FILE_PATTERN_BINARY = "*.exe"
+        #----------------------------------------------------------------------
+        # the name of the path that will contain built 32-bit binary files
+        _PATH_NAME_BINARY_X86 = "..\\sdk\\x86\\bin"
+        #----------------------------------------------------------------------
+        # the name of the path that will contain built 64-bit binary files
+        _PATH_NAME_BINARY_X64 = "..\\sdk\\x64\\bin"
+        #----------------------------------------------------------------------
+        # the name of the path that will contain intermediary build files
+        _PATH_NAME_BUILD = "Newmat"
+        #----------------------------------------------------------------------
+        # the name of the path that will contain built 32-bit library files
+        _PATH_NAME_DISTRIBUTION_X86 = "..\\sdk\\x86\\lib"
+        #----------------------------------------------------------------------
+        # the name of the path that will contain built 64-bit library files
+        _PATH_NAME_DISTRIBUTION_X64 = "..\\sdk\\x64\\lib"
+        #----------------------------------------------------------------------
+        # the name of the path that contains the source code
+        _PATH_NAME_SOURCE = "..\\src\\Newmat"
+        #----------------------------------------------------------------------
+        
+    #--------------------------------------------------------------------------
+    # constructors
+    
+        #----------------------------------------------------------------------
+        # Constructs this program.
+        #
+        # Parameters :
+        #     self : this program
+        def __init__(self) :
+        
+            pass
+        #----------------------------------------------------------------------
+        
+    #--------------------------------------------------------------------------
+    # public methods
+    
+        #----------------------------------------------------------------------
+        # The main method of the program.
+        #
+        # Parameters :
+        #     self : this program
+        def main(self) :
+        
+            systemManager = SystemManager()
+            pathFinder    = PathFinder()
+            
+            # process command-line arguments
+            buildSettings = BuildSettingSet.fromCommandLine(Program.DESCRIPTION)
+            
+            # initialize environment variables
+            systemManager.initializeIncludeEnvironmentVariable( buildSettings.X64Specified() )
+            systemManager.initializeLibraryEnvironmentVariable( buildSettings.X64Specified() )
+            systemManager.appendToPathEnvironmentVariable( pathFinder.getVisualStudioBinPathName( buildSettings.X64Specified() ) )
+
+            # determine path names
+            binaryPathName = ( systemManager.getCurrentRelativePathName(Program._PATH_NAME_BINARY_X64) \
+                               if ( buildSettings.X64Specified() )                                     \
+                               else systemManager.getCurrentRelativePathName(Program._PATH_NAME_BINARY_X86) )
+            buildPathName  = systemManager.getCurrentRelativePathName(Program._PATH_NAME_BUILD)
+            sourcePathName = systemManager.getCurrentRelativePathName(Program._PATH_NAME_SOURCE)
+            
+            # determine file names
+            if ( buildSettings.ReleaseSpecified() and \
+                 buildSettings.X64Specified()       ) :
+                 
+                 buildLibraryFileName        = os.path.join( buildPathName                                                                 , \
+                                                             Program._FILE_NAME_LIBRARY_BUILD_RELEASE                                      ) 
+                 distributionLibraryFileName = os.path.join( systemManager.getCurrentRelativePathName(Program._PATH_NAME_DISTRIBUTION_X64) , \
+                                                             Program._FILE_NAME_LIBRARY_DISTRIBUTION_RELEASE                               )
+                 
+            elif ( buildSettings.ReleaseSpecified() ) :
+            
+                 buildLibraryFileName        = os.path.join( buildPathName                                                                 , \
+                                                             Program._FILE_NAME_LIBRARY_BUILD_RELEASE                                      ) 
+                 distributionLibraryFileName = os.path.join( systemManager.getCurrentRelativePathName(Program._PATH_NAME_DISTRIBUTION_X86) , \
+                                                             Program._FILE_NAME_LIBRARY_DISTRIBUTION_RELEASE                               )
+                 
+            elif ( buildSettings.X64Specified() ) :
+            
+                 buildLibraryFileName        = os.path.join( buildPathName                                                                 , \
+                                                             Program._FILE_NAME_LIBRARY_BUILD_DEBUG                                        ) 
+                 distributionLibraryFileName = os.path.join( systemManager.getCurrentRelativePathName(Program._PATH_NAME_DISTRIBUTION_X64) , \
+                                                             Program._FILE_NAME_LIBRARY_DISTRIBUTION_DEBUG                                 )
+                 
+            else :
+            
+                 buildLibraryFileName        = os.path.join( buildPathName                                                                 , \
+                                                             Program._FILE_NAME_LIBRARY_BUILD_DEBUG                                        ) 
+                 distributionLibraryFileName = os.path.join( systemManager.getCurrentRelativePathName(Program._PATH_NAME_DISTRIBUTION_X86) , \
+                                                             Program._FILE_NAME_LIBRARY_DISTRIBUTION_DEBUG                                 )
+            
+            # initialize directories
+            systemManager.changeDirectory(sourcePathName)
+            systemManager.removeDirectory(buildPathName)
+            systemManager.copyDirectory( sourcePathName ,
+                                         buildPathName  )
+                                         
+            # execute Nmake
+            nmakeCommandLine = ( "\"%s\" /f \"%s\" _MSC_VER=1900" % \
+                                 ( pathFinder.getNmakeFileName( buildSettings.X64Specified() ) , \
+                                   Program._FILE_NAME_MAKEFILE                                 ) )
+            if ( buildSettings.ReleaseSpecified() ) :
+            
+                 nmakeCommandLine += " nodebug=1"
+            
+            systemManager.changeDirectory(buildPathName)
+            nmakeResult = systemManager.execute(nmakeCommandLine)
+                
+            # distribute the library files
+            systemManager.copyFile( buildLibraryFileName        , \
+                                    distributionLibraryFileName )
+
+            # copy the binary files
+            buildBinaryFileNames = glob.glob( os.path.join( buildPathName                , \
+                                                            Program._FILE_PATTERN_BINARY ) );
+            for buildBinaryFileName in buildBinaryFileNames :
+            
+                binaryFileName = os.path.join( binaryPathName , \
+                                               os.path.basename(buildBinaryFileName) )
+                systemManager.copyFile( buildBinaryFileName , \
+                                        binaryFileName      )
+        #----------------------------------------------------------------------
+        
+    #--------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+Program().main()
+#------------------------------------------------------------------------------
