0800eadd6fb2231225575d9528d710d431848538
Beth Okun <BethOkun@gmail.com>
2016-09-11 20:06:03 -0700

    Tweaked return value code in UriParser and Expat

--- tests/Expat/RunApp.cpp
+++ tests/Expat/RunApp.cpp
@@ -20,10 +20,6 @@ int main(int argc, char* argv[])
 	bool wasSucessful = runner.run();
 
 	// Return error code 1 if the one of test failed.
-	if (wasSucessful) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
+	return wasSucessful ? 0 : 1;
+
 }
