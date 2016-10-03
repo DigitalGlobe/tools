fb89cdbf59f6d12a53de72cb4c4018754463e62b
Beth Okun <BethOkun@gmail.com>
2016-10-03 05:19:14 -0700

    Removed previous Geos builds and scripts, added new Visual C++ GEOS library build and build script

--- build/build.py
+++ build/build.py
@@ -38,6 +38,9 @@ class Program :
         # the name of the build file to build FreeType
         _FILE_NAME_BUILD_GALIB = "build_galib.py"
         #----------------------------------------------------------------------
+         # the name of the build file to build Geos
+        _FILE_NAME_BUILD_GEOS = "build_geos.py"
+        #----------------------------------------------------------------------
         # the name of the build file to build FreeType
         _FILE_NAME_BUILD_GLUT = "build_glut.py"
         #----------------------------------------------------------------------
@@ -174,6 +177,7 @@ class Program :
                 self._build(Program._FILE_NAME_BUILD_CURL       )
                 self._build(Program._FILE_NAME_BUILD_EXPAT      )
                 self._build(Program._FILE_NAME_BUILD_FREETYPE   )
+                self._build(Program._FILE_NAME_BUILD_GEOS       )
                 self._build(Program._FILE_NAME_BUILD_GSOAP      )
                 self._build(Program._FILE_NAME_BUILD_HDF5       )
                 self._build(Program._FILE_NAME_BUILD_KDIS       )
