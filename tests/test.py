abfa957435ce5a873bc09648feb42d1849ccde6a
Beth Okun <BethOkun@gmail.com>
2016-10-31 06:51:02 -0700

    build script for test cases of QWT project - everything working except a jom compiler error

--- tests/test.py
+++ tests/test.py
@@ -95,6 +95,9 @@ class Program :
         # the name of the test file to test PROJ4
         _FILE_NAME_TEST_PROJ4 = "test_proj4.py"
         #----------------------------------------------------------------------
+        # the name of the test file to test qwt
+        _FILE_NAME_TEST_QWT = "test_qwt.py"
+        #----------------------------------------------------------------------
         # the name of the test file to test SZip
         _FILE_NAME_TEST_SZIP = "test_szip.py"
         #----------------------------------------------------------------------
@@ -165,6 +168,7 @@ class Program :
                 self._test(Program._FILE_NAME_TEST_OPENTHREADS)
                 self._test(Program._FILE_NAME_TEST_PODOFO     )
                 self._test(Program._FILE_NAME_TEST_PROJ4      )
+                self._test(Program._FILE_NAME_TEST_QWT        )
                 self._test(Program._FILE_NAME_TEST_SZIP       )
                 self._test(Program._FILE_NAME_TEST_URIPARSER  )
                 self._test(Program._FILE_NAME_TEST_VLD        )
