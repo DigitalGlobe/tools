9982be7c40427e32c4a6e85b2e5cccaf34f070e4
tim.tisler <tim.tisler@maxar.com>
2025-06-23 11:47:56 -0400

    updated for build_glew

--- build/build_glew.py
+++ build/build_glew.py
@@ -24,6 +24,7 @@ class Program:
     # --------------------------------------------------------------------------
     # constants
 
+    DESCRIPTION = "Builds the GLEW library."
     # ----------------------------------------------------------------------
     # the name of the path that will contain built 32-bit binary files
     _PATH_NAME_BINARY_X86 = "..\\sdk\\x86\\bin"
@@ -32,7 +33,7 @@ class Program:
     _PATH_NAME_BINARY_X64 = "..\\sdk\\x64\\bin"
     # ----------------------------------------------------------------------
     # the name of the path that will contain intermediary build files
-    _PATH_NAME_BUILD = "geos"
+    _PATH_NAME_BUILD = "glew"
     # ----------------------------------------------------------------------
     # the name of the path that will contain built 32-bit library files
     _PATH_NAME_DISTRIBUTION_X86 = "..\\sdk\\x86\\lib"
@@ -41,21 +42,20 @@ class Program:
     _PATH_NAME_DISTRIBUTION_X64 = "..\\sdk\\x64\\lib"
     # ----------------------------------------------------------------------
     # the name of the path that contains the source code
-    _PATH_NAME_SOURCE = "..\\src\\geos"
+    _PATH_NAME_SOURCE = "..\\src\\glew"
 
     # ----------------------------------------------------------------------
     # the base name of the library
-    _LIBNAME = "geos"
-    _LIBNAME2 = "geos_c"
+    _LIBNAME = "glew"
     _DEBUG_SUFFIX = "_d"
 
     # ----------------------------------------------------------------------
     # the name of the distribution path for all include files
-    _PATH_NAME_DISTRIBUTION_INCLUDE = "..\\..\\include\\geos"
+    _PATH_NAME_DISTRIBUTION_INCLUDE = "..\\..\\include\\glew"
     # ----------------------------------------------------------------------
     # the name of the path that contains the cmake files
     _PATH_NAME_CMAKE_SOURCE = "."
-    _PATH_NAME_CMAKE_BUILD = "build"
+    _PATH_NAME_CMAKE_BUILD = "build\\build-glew"
     _PATH_NAME_CMAKE_INSTALL = "install"
 
     def __init__(self):
@@ -132,10 +132,10 @@ class Program:
         cmakeCommandLine = (
             f'{pathFinder.getCMakeFileName()} -G "{pathFinder.VISUAL_STUDIO_VERSION}" '
             + f"-A {platform} "
-            + f"-DBUILD_DOCUMENTATION=OFF "
+            + f"-DBUILD_UTILS=OFF "
             + f"-DBUILD_SHARED_LIBS=ON "
-            + f"-DBUILD_TESTING=OFF "
-            + f"{buildSourceName}"
+            + f"-DCMAKE_POLICY_VERSION_MINIMUM=3.16 "
+            + f"{buildSourceName}/build/cmake"
         )
 
         print("cmake: " + cmakeCommandLine)
@@ -147,6 +147,7 @@ class Program:
             f"{pathFinder.getCMakeFileName()} "
             + f"--build "
             + f". "
+            + f"--target glew "
             + f"--config {conf} "
         )
 
@@ -179,26 +180,19 @@ class Program:
 
         dllName = (
             f"{Program._LIBNAME}"
-            + f'{"" if (buildSettings.ReleaseSpecified()) else Program._DEBUG_SUFFIX}'
-            + f".dll"
-        )
-        dllName2 = (
-            f"{Program._LIBNAME2}"
+            + f'{"64" if buildSettings.X64Specified() else "32"}'
             + f'{"" if (buildSettings.ReleaseSpecified()) else Program._DEBUG_SUFFIX}'
             + f".dll"
         )
         libName = (
             f"{Program._LIBNAME}"
-            + f'{"" if (buildSettings.ReleaseSpecified()) else Program._DEBUG_SUFFIX}'
-            + f".lib"
-        )
-        libName2 = (
-            f"{Program._LIBNAME2}"
+            + f'{"64" if buildSettings.X64Specified() else "32"}'
             + f'{"" if (buildSettings.ReleaseSpecified()) else Program._DEBUG_SUFFIX}'
             + f".lib"
         )
         pdbName = (
             f"{Program._LIBNAME}"
+            + f'{"64" if buildSettings.X64Specified() else "32"}'
             + f'{"" if (buildSettings.ReleaseSpecified()) else Program._DEBUG_SUFFIX}'
             + f".pdb"
         )
@@ -207,7 +201,7 @@ class Program:
             os.path.join(
                 cmakeInstallPath,
                 "lib",
-                f"{Program._LIBNAME}.lib",
+                f"{Program._LIBNAME}{'64' if buildSettings.X64Specified() else '32'}{'' if (buildSettings.ReleaseSpecified()) else 'd'}.lib",
             ),
             os.path.join(sdkOutDir, libName),
         )
@@ -215,27 +209,10 @@ class Program:
         systemManager.copyFile(
             os.path.join(
                 cmakeInstallPath,
-                "lib",
-                f"{Program._LIBNAME2}.lib",
-            ),
-            os.path.join(sdkOutDir, libName2),
-        )
-
-        systemManager.copyFile(
-            os.path.join(
-                cmakeInstallPath,
                 "bin",
-                f"{Program._LIBNAME}.dll",
+                f"{Program._LIBNAME}{'64' if buildSettings.X64Specified() else '32'}{'' if (buildSettings.ReleaseSpecified()) else 'd'}.dll",
             ),
             os.path.join(sdkOutDir, dllName),
-        )
-        systemManager.copyFile(
-            os.path.join(
-                cmakeInstallPath,
-                "bin",
-                f"{Program._LIBNAME2}.dll",
-            ),
-            os.path.join(sdkOutDir, dllName2),
         )
 
         # for some reason, the pdb doesn't get installed during "cmake --install"
@@ -243,10 +220,8 @@ class Program:
             systemManager.copyFile(
                 os.path.join(
                     cmakeInstallPath,
-                    "..",
-                    "bin",
-                    "Debug",
-                    f"{Program._LIBNAME}.pdb",
+                    "lib",
+                    f"{Program._LIBNAME}{'64' if buildSettings.X64Specified() else '32'}{'' if (buildSettings.ReleaseSpecified()) else 'd'}.pdb",
                 ),
                 os.path.join(sdkOutDir, pdbName),
             )
