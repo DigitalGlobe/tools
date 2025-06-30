4864cd18e08c9df41059528cc768b8e2c8cd116b
tim.tisler <tim.tisler@maxar.com>
2025-06-30 11:18:55 -0400

    update build_libiconv

--- build/build_libiconv.py
+++ build/build_libiconv.py
@@ -1,10 +1,10 @@
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 #
 # build_libiconv.py
 #
 # Summary : Builds the LibICONV library.
 #
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 import glob
 import os
@@ -15,144 +15,179 @@ from PathFinder import *
 from SystemManager import *
 from XmlUtils import *
 
-class Program :
-        #----------------------------------------------------------------------
-        # a description of what the script does
-        DESCRIPTION = "Builds the libICONV library."
-        #----------------------------------------------------------------------
-        # the name of the dynamic solution file
-        _FILE_NAME_SOLUTION = "libiconv\\libiconv.vcxproj"
-        
-        #----------------------------------------------------------------------
-        # the name of the path that will contain intermediary build files
-        _PATH_NAME_BUILD = "libICONV"
-        #----------------------------------------------------------------------
-        # the name of the path that contains the source code
-        _PATH_NAME_SOURCE = "..\\src\\libICONV"
-        #----------------------------------------------------------------------
-                
-        _PATH_NAME_DISTRIBUTION_X86 = "..\\sdk\\x86\\lib"
-        _PATH_NAME_DISTRIBUTION_X64 = "..\\sdk\\x64\\lib"
-        
-        _LIBNAME = 'libiconv'
-        _DEBUG_SUFFIX = '_d'
 
-        # the name of the path for all include files
-        _PATH_NAME_INCLUDE = 'include'
-        #----------------------------------------------------------------------
-        # the name of the distribution path for all include files
-        _PATH_NAME_DISTRIBUTION_INCLUDE = '..\\..\\include\\libiconv'
-        #----------------------------------------------------------------------
+class Program:
+    # ----------------------------------------------------------------------
+    # a description of what the script does
+    DESCRIPTION = "Builds the libICONV library."
+    # ----------------------------------------------------------------------
 
-        def __init__(self) :
-        
-            pass
-        #----------------------------------------------------------------------
-        
+    # ----------------------------------------------------------------------
+    # the name of the path that will contain intermediary build files
+    _PATH_NAME_BUILD = "libICONV"
+    # ----------------------------------------------------------------------
+    # the name of the path that contains the source code
+    _PATH_NAME_SOURCE = "..\\src\\libICONV"
+    # ----------------------------------------------------------------------
 
-        def main(self) :
-            systemManager = SystemManager()
-            pathFinder    = PathFinder()
-            xmlUtils = XmlUtils()
-            
-            # process command-line arguments
-            buildSettings = BuildSettingSet.fromCommandLine(Program.DESCRIPTION)
-            
-            # initialize environment variables
-            systemManager.initializeIncludeEnvironmentVariable( buildSettings.X64Specified() )
-            systemManager.initializeLibraryEnvironmentVariable( buildSettings.X64Specified() )
-            systemManager.appendToPathEnvironmentVariable( pathFinder.getVisualStudioBinPathName( buildSettings.X64Specified() ) )
+    _PATH_NAME_DISTRIBUTION_X86 = "..\\sdk\\x86\\lib"
+    _PATH_NAME_DISTRIBUTION_X64 = "..\\sdk\\x64\\lib"
 
-            # MSBuild is under "Program Files (x86)"
-            systemManager.appendToPathEnvironmentVariable( os.path.join( systemManager.getProgramFilesPathName(False) , \
-                                                                             "MSBuild\\14.0\\Bin") )
+    _LIBNAME = "libiconv"
+    _DEBUG_SUFFIX = "_d"
 
-            compileOutDir = ""
-            if ( buildSettings.X64Specified() ) :
-                # append path for 64-bit rc.exe
-                systemManager.appendToPathEnvironmentVariable( os.path.join( systemManager.getProgramFilesPathName(False) , \
-                                                                             "Windows Kits\\10\\bin\\x64"                 ) )
-            else:
-                # append path for 32-bit rc.exe
-                systemManager.appendToPathEnvironmentVariable( os.path.join( systemManager.getProgramFilesPathName(False) , \
-                                                                             "Windows Kits\\10\\bin\\x86"                 ) )
+    # the name of the path for all include files
+    _PATH_NAME_INCLUDE = "include"
+    _PATH_NAME_BIN = "bin"
+    _PATH_NAME_LIB = "lib"
+    # ----------------------------------------------------------------------
+    # the name of the distribution path for all include files
+    _PATH_NAME_DISTRIBUTION_INCLUDE = "..\\..\\include\\libiconv"
+    # ----------------------------------------------------------------------
+    _PATH_NAME_NMAKE_INSTALL = "install"
 
-            # get the paths
-            buildPathName  = systemManager.getCurrentRelativePathName(Program._PATH_NAME_BUILD)
-            sourcePathName = systemManager.getCurrentRelativePathName(Program._PATH_NAME_SOURCE)
-            sdkOutDir = buildPathName + "\\..\\" + (Program._PATH_NAME_DISTRIBUTION_X64 if buildSettings.X64Specified() else Program._PATH_NAME_DISTRIBUTION_X86)
+    def __init__(self):
 
-            # remove build dir
-            systemManager.changeDirectory(sourcePathName)
-            systemManager.removeDirectory(buildPathName)
+        pass
 
-            #copy UriParser to the Build area
-            systemManager.copyDirectory( sourcePathName, buildPathName)
-        
-            # start building
-            systemManager.changeDirectory(buildPathName)
-                                    
-            
-            conf = "Release" if ( buildSettings.ReleaseSpecified() ) else "Debug"
-            platform  = 'x64' if buildSettings.X64Specified() else 'Win32'
-            # build the solution
-            solutionFileName   = os.path.join( buildPathName               , \
-                                               Program._FILE_NAME_SOLUTION )
-            msBuildCommandLine = ( "\"%s\" "                  + \
-                                     "/p:platform=%s " + \
-                                     "\"%s\""                   ) % \
-                                   ( pathFinder.getMSBuildFileName( buildSettings.X64Specified()), platform, solutionFileName )
-            
-            dllName = Program._LIBNAME + ( '' if ( buildSettings.ReleaseSpecified() )  else Program._DEBUG_SUFFIX) + ".dll"
-            libName = Program._LIBNAME + ( '' if ( buildSettings.ReleaseSpecified() )  else Program._DEBUG_SUFFIX) + ".lib"
-            pdbName = Program._LIBNAME + ( '' if ( buildSettings.ReleaseSpecified() )  else Program._DEBUG_SUFFIX) + ".pdb"
+    # ----------------------------------------------------------------------
 
-            buildOutDir   = os.path.join( buildPathName, 'build' )
-            propfile   = os.path.join( buildPathName, 'linker.props' )
-            
-            msBuildCommandLine += ' /p:OutDir=' + buildOutDir
-            msBuildCommandLine += ' /p:TargetExtension=dll'
-            msBuildCommandLine += ' /p:SolutionDir=' + buildPathName + '\\' 
-            msBuildCommandLine += ' /p:TargetName=' + Program._LIBNAME + ('' if ( buildSettings.ReleaseSpecified() )  else Program._DEBUG_SUFFIX)
-            msBuildCommandLine += ' /p:Configuration=' + conf
-            msBuildCommandLine += ' /p:BuildProjectReferences=false'
+    def main(self):
+        systemManager = SystemManager()
+        pathFinder = PathFinder()
+
+        # process command-line arguments
+        buildSettings = BuildSettingSet.fromCommandLine(Program.DESCRIPTION)
+
+        # initialize environment variables
+        systemManager.initializeIncludeEnvironmentVariable(buildSettings.X64Specified())
+        systemManager.initializeLibraryEnvironmentVariable(buildSettings.X64Specified())
+
+        # systemManager.setEnvironmentVariableValue("VS170COMNTOOLS",
+
+        # MSBuild is under "Program Files (x86)"
+        systemManager.appendToPathEnvironmentVariable(
+            pathFinder.getMSBuildFileName(buildSettings.X64Specified())
+        )
+
+        vcVars = pathFinder.getVCVARSFileName(buildSettings.X64Specified())
+
+        compileOutDir = ""
+        systemManager.appendToPathEnvironmentVariable(
+            pathFinder.getWindowsSdkBinPathName(buildSettings.X64Specified())
+        )
+
+        # get the paths
+        buildPathName = systemManager.getCurrentRelativePathName(
+            Program._PATH_NAME_BUILD
+        )
+        sourcePathName = systemManager.getCurrentRelativePathName(
+            Program._PATH_NAME_SOURCE
+        )
+
+        nmakeInstallPath = os.path.join(buildPathName, Program._PATH_NAME_NMAKE_INSTALL)
+
+        sdkOutDir = os.path.join(
+            buildPathName,
+            "..",
+            (
+                Program._PATH_NAME_DISTRIBUTION_X64
+                if buildSettings.X64Specified()
+                else Program._PATH_NAME_DISTRIBUTION_X86
+            ),
+        )
+
+        # remove build dir
+        systemManager.removeDirectory(buildPathName)
+
+        # copy source to the Build area
+        systemManager.copyDirectory(sourcePathName, buildPathName)
+        systemManager.changeDirectory(buildPathName)
+
+        systemManager.copyFile(
+            os.path.join(buildPathName, "include", "iconv.h.msvc-shared"),
+            os.path.join(buildPathName, "include", "iconv.h"),
+        )
+        # start building
+        srcdir = os.path.join(buildPathName, "lib")
+
+        installpath = os.path.join(buildPathName, Program._PATH_NAME_NMAKE_INSTALL)
+
+        # run Nmake
+        nmakeCommandLine = (
+            f"nmake -f Makefile.msvc "
+            + f"DLL=1 "
+            + f"NO_NLS=1 "
+            + f"DEBUG={'0' if buildSettings.ReleaseSpecified() else "1"} "
+            + f"PREFIX={installpath} IIPREFIX={installpath} "
+        )
+
+        # cmd = f'"{vcVars}" && {nmakeCommandLine} config.h'
+        # print("cmd: " + cmd)
+        # nmakeResult = systemManager.execute(cmd)
+        # if nmakeResult != 0:
+        #     sys.exit(-1)
+
+        # systemManager.changeDirectory(srcdir)
+        cmd = f'"{vcVars}" && {nmakeCommandLine} all install'
+        print("cmd: " + cmd)
+        nmakeResult = systemManager.execute(cmd)
+        if nmakeResult != 0:
+            sys.exit(-1)
+
+        systemManager.removeDirectory(
+            os.path.join(buildPathName, Program._PATH_NAME_DISTRIBUTION_INCLUDE)
+        )
+
+        systemManager.copyFile(
+            os.path.join(
+                buildPathName,
+                Program._PATH_NAME_NMAKE_INSTALL,
+                Program._PATH_NAME_INCLUDE,
+                "iconv.h",
+            ),
+            os.path.join(buildPathName, Program._PATH_NAME_DISTRIBUTION_INCLUDE, "iconv.h")
+        )
+        systemManager.copyFile(
+            os.path.join(
+                buildPathName,
+                Program._PATH_NAME_NMAKE_INSTALL,
+                Program._PATH_NAME_BIN,
+                "iconv.dll",
+            ),
+            os.path.join(
+                sdkOutDir,
+                f'{Program._LIBNAME}{"" if buildSettings.ReleaseSpecified() else Program._DEBUG_SUFFIX}.dll',
+            ),
+        )
+        systemManager.copyFile(
+            os.path.join(
+                buildPathName,
+                Program._PATH_NAME_NMAKE_INSTALL,
+                Program._PATH_NAME_LIB,
+                "iconv.lib",
+            ),
+            os.path.join(
+                sdkOutDir,
+                f'{Program._LIBNAME}{"" if buildSettings.ReleaseSpecified() else Program._DEBUG_SUFFIX}.lib',
+            ),
+        )
+
+        if not buildSettings.ReleaseSpecified():
+            systemManager.copyFile(
+                os.path.join(
+                    buildPathName,
+                    Program._PATH_NAME_LIB,
+                    "iconv.pdb",
+                ),
+                os.path.join(
+                    buildPathName,
+                    sdkOutDir,
+                    f'{Program._LIBNAME}{"" if buildSettings.ReleaseSpecified() else Program._DEBUG_SUFFIX}.pdb',
+                ),
+            )
 
 
-            linkerprops = {'OutputFile':os.path.join( buildOutDir, dllName )}
-            linkerprops['ImportLibrary'] = os.path.join( buildOutDir, libName )
-            if buildSettings.ReleaseSpecified():
-                linkerprops['DebugSymbols'] = 'false'
-            else:
-                linkerprops['DebugSymbols'] = 'true'
-                linkerprops['DebugType'] = 'full'
-                linkerprops['ProgramDatabaseFile'] = '$(OutDir)\\' + pdbName
-              
-            xmlUtils.buildDll(conf, platform, {}, linkerprops, propfile)
-
-            msBuildCommandLine +=  ' /p:ForceImportBeforeCppTargets="' + propfile + '"'
-            
-
-            print('cmd: ' + msBuildCommandLine)
-                       
-            msbuildResult = systemManager.execute(msBuildCommandLine)
-            if (msbuildResult != 0) :
-                sys.exit(-1)
-
-
-            systemManager.removeDirectory(os.path.join( buildPathName, Program._PATH_NAME_DISTRIBUTION_INCLUDE))
-            systemManager.distributeFiles(os.path.join( buildPathName, Program._PATH_NAME_INCLUDE),              \
-                              os.path.join( buildPathName, Program._PATH_NAME_DISTRIBUTION_INCLUDE), \
-                              'iconv.h',                                                                 \
-                              True, False) 
-                
-            systemManager.copyFile( os.path.join( buildOutDir, libName ) , \
-                                    os.path.join( sdkOutDir , libName) )
-            systemManager.copyFile( os.path.join( buildOutDir, dllName ) , \
-                                    os.path.join( sdkOutDir , dllName) )
-            if not buildSettings.ReleaseSpecified():
-                systemManager.copyFile( os.path.join( buildOutDir, pdbName ) , \
-                                        os.path.join( sdkOutDir , pdbName) )
-
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 Program().main()
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
