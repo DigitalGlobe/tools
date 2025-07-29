335d35d75c330b873c7d4efe60a5b53ddf2c10f8
tim.tisler <tim.tisler@maxar.com>
2025-07-29 11:52:14 -0400

    update quazip to v1.5

--- src/QuaZIP/quazip/quazip/run_moc.bat
+++
@@ -1,3 +0,0 @@
-C:\Qt\Qt5.7.0_x86\5.7\msvc2015\bin\moc.exe -o moc\moc_quazipfile.cpp quazipfile.h
-C:\Qt\Qt5.7.0_x86\5.7\msvc2015\bin\moc.exe -o moc\moc_quagzipfile.cpp quagzipfile.h
-C:\Qt\Qt5.7.0_x86\5.7\msvc2015\bin\moc.exe -o moc\moc_quaziodevice.cpp quaziodevice.h
