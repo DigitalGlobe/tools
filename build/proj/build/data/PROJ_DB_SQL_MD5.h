5d96c69ae8481c445455fc13f9f04f217fa353f4
tim.tisler <tim.tisler@maxar.com>
2025-08-01 07:08:56 -0400

    remove proj & sqlite3 from build

--- build/proj/build/data/PROJ_DB_SQL_MD5.h
+++
@@ -1,1 +0,0 @@
-const char* PROJ_DB_SQL_MD5="fff8fb31e3058ea95a6a5cda79ff8070";
--- build/proj/data/sql/customizations_early.sql
+++
@@ -1,8 +0,0 @@
--- This file is hand generated.
-
-INSERT INTO "extent" VALUES('PROJ','EXTENT_UNKNOWN','Not specified','Not specified.',-90.0,90.0,-180.0,180.0,0);
-INSERT INTO "scope" VALUES('PROJ','SCOPE_UNKNOWN','Not known.',0);
-
-INSERT INTO celestial_body VALUES('PROJ', 'EARTH', 'Earth', 6378137.0);
-
-INSERT INTO versioned_auth_name_mapping VALUES ('IAU_2015', 'IAU', '2015', 1);
